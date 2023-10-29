64cf539f13cf3f3dcbaa3cd0d9b03eea39d10569
Brandon Ting <brandon1221t@gmail.com>
2023-10-29 21:00:38 +0800

    simple reference ui

--- src/components/reference.tsx
+++ src/components/reference.tsx
@@ -1,7 +1,27 @@
 
 
 export default function reference() {
+    const referenceList = [
+        "原食譜",
+        "料理影片",
+        "xx購買連結"
+    ]
     return (
-        <div>reference</div>
+        <div class="mt-6 space-y-8">
+            <div class="flex flex-col space-y-4 m-auto  min-w-fit w-1/4">
+                <ul>
+                    {
+                        referenceList.map(link => (
+                            <li>
+                                <a href="#">
+                                    <h2 class="text-blue-500 text-2xl border-b-2">{link}</h2>
+                                </a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+
     )
 }
