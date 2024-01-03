1547c01bbe13a4f9400acef122933fd43e5797ad
Brandon Ting <brandon1221t@gmail.com>
2024-01-03 09:27:25 +0800

    fix tags style

--- src/components/tags.tsx
+++ src/components/tags.tsx
@@ -1,3 +1,12 @@
+import { twMerge } from "tailwind-merge";
+
+const TAG_COLOR_OPTIONS = [
+    ["bg-green-200", "text-green-800"],
+    ["bg-blue-200", "text-blue-800"],
+    ["bg-indigo-200", "text-indigo-800"],
+    ["bg-purple-200", "text-purple-800"],
+];
+
 export interface TagsProps {
     tags: string[];
 }
@@ -5,11 +14,13 @@ export interface TagsProps {
 export function Tags({ tags }: TagsProps) {
     return (
         <div class="flex gap-1 justify-center">
-            {tags.map((tag) => (
-                <span class="inline-block h-6 rounded-full bg-green-200 px-2 text-xs font-semibold uppercase leading-6 tracking-wide text-green-800">
-                    {tag}
-                </span>
-            ))}
+            {tags.map((tag, i) => {
+                const className = twMerge(
+                    "inline-block h-6 rounded-full px-2 text-xs font-semibold uppercase leading-6 tracking-wide",
+                    TAG_COLOR_OPTIONS[i % 4],
+                );
+                return <span class={className}>{tag}</span>;
+            })}
         </div>
     );
 }
