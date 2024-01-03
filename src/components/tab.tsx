7f54373bf68e9342551355ceb0b51fa7108ece3b
Brandon Ting <brandon1221t@gmail.com>
2024-01-03 09:34:00 +0800

    use tw merge

--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from "tailwind-merge";
+
 export interface TabProps {
     label: string;
     type: string;
@@ -5,15 +7,17 @@ export interface TabProps {
 }
 
 export default function ({ label, active, type }: TabProps) {
-    const activeClass = active
-        ? "border-blue-600 text-blue-600 dark:border-blue-500 dark:text-blue-500"
-        : "border-transparent hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300";
     return (
         <li
             hx-get={`/api/detail/content/1?type=${type}`}
             hx-target="next div"
             hx-swap="innerHTML"
-            class={`${activeClass} inline-block cursor-pointer rounded-t-lg border-b-2  p-4`}
+            class={twMerge(
+                "inline-block cursor-pointer rounded-t-lg border-b-2  p-4",
+                active
+                    ? "border-blue-600 text-blue-600 dark:border-blue-500 dark:text-blue-500"
+                    : "border-transparent hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300",
+            )}
         >
             {label}
         </li>
