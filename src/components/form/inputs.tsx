af6ee2e74258f8a0b6aa8a8fa2ea8293769dd7b8
Brandon Ting <brandon1221t@gmail.com>
2024-04-04 15:34:12 +0800

    remove required fields

--- src/components/form/inputs.tsx
+++ src/components/form/inputs.tsx
@@ -71,12 +71,6 @@ export function StepInput({ count }: { count: number }) {
                     name="stepTitle"
                     class="mb-2 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
-                <textarea
-                    placeholder="請簡述食譜步驟"
-                    name="stepDescription"
-                    id="stepDescription"
-                    class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
             </div>
             <div>
                 <button
